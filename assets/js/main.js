bd2c68ead6bbcd35267cdea3388f0f3fbd95ee32
Eriksen <eriksen.lezama@gmail.com>
2018-03-12 15:48:35 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,106 @@ $(document).ready(function(){
       }
       cero = $(window).scrollTop();
    });
-});
+
+   // ---------
+   // portfilio click events
+   $('.image img').on('click', function(){
+      // object image
+      var arrayElements = $('.image img');
+      //  src image
+      var src = arrayElements.index(this);
+
+      // show overlay div with image
+      $('#overlay').css("display", "block");
+      // add src image in img tag
+      $('#over-img').attr("src", arrayElements[src].src);
+
+      // click previous
+      var previous = document.getElementById('previous');
+      // click next
+      var next = document.getElementById('next');
+
+      // Listener previous
+      previous.addEventListener('click', previousClick);
+      // Listener next
+      next.addEventListener('click', nextClick);
+
+      function previousClick(){
+         if(src == 0){
+            src = arrayElements.length - 1;
+            setImage;
+         } else {
+            src -= 1;
+            $('#over-img').attr("src", arrayElements[src].src);
+         }
+      }//Fin next click
+      function nextClick(){
+         if(src == arrayElements.length - 1){
+            src = 0;
+            $('#over-img').attr("src", arrayElements[src].src);
+         } else {
+            src += 1;
+            $('#over-img').attr("src", arrayElements[src].src);
+         }
+      }//Fin next click
+
+      // Div Overlay
+      var overlay = document.getElementById('overlay');
+      // listener event click, when click out of the img
+      var fullImage = document.getElementById('full-image');
+      // container image and buttons next previous
+      var fImg = document.getElementById('f-img');
+      // close button
+      var close = document.getElementById('close');
+      // image tag
+      var coverImage = document.getElementById('cover-image');
+      // listener event click, when click out of the img
+      window.addEventListener('click', clickOutk);
+      // listener event click, when click out of the img
+      function clickOutk(e){
+         if((e.target == close ||e.target == overlay || e.target == fullImage || e.target == fImg) && !(e.target == coverImage)){
+            overlay.style.display = "none";
+         }
+      }//fin clickOut
+
+   });//  fin .image img (click)
+
+   $('#filter-ritratti').on('click', function(e){
+      $('.image.matrimonio, .image.moda, .image.ritratti').css("display", "none");
+      $('.image.ritratti').css("display", "block");
+      e.preventDefault();
+   });// fin click filter ritratti
+
+   $('#filter-matrimonio').on('click', function(e){
+      $('.image.matrimonio, .image.moda, .image.ritratti').css("display", "none");
+      /*$.get('matrimonio-portfolio.txt', function (ob) {
+         var txtArray = ob.split(" ");
+         var images = {};
+         images = $('.image.matrimonio img');
+
+         for (var key in images) {
+            if (images.hasOwnProperty(key)) {
+               
+               for (var i in txtArray) {
+                  images[key].src = txtArray[i];
+                  console.log(i + " -> " + txtArray[i]);
+                  console.log(key + " -> " + images[key].src);
+
+               }
+            }
+         }
+
+      });  */
+      $('.image.matrimonio').css("display", "block");
+      $('#del').css("display", "none");
+      e.preventDefault();
+
+   });// fin click filter ritratti
+
+   $('#filter-moda').on('click', function(e){
+      $('.image.matrimonio, .image.moda, .image.ritratti').css("display", "none");
+      $('.image.moda').css("display", "block");
+      e.preventDefault();
+   });// fin click filter ritratti
+
+});// Document ready
